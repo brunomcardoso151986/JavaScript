671fddc18095f51709e9fd76a699d3365aa75190
brunomcardoso151986 <brunomcardoso151986@gmail.com>
2020-07-28 12:03:30 -0300

    commit condicoesP1.js
    Testando o JavaScript em um arquivo externo.

---
+++ condicoesP1.js
@@ -0,0 +1,17 @@
+/*
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} km/h.`)
+if (vel > 60) { //Condição simples
+    console.log('Você ultrapassou o limite de velocidade da via. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
+*/
+
+var pais = 'Brasil'
+console.log(`Vivendo no ${pais}.`)
+
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro.')    
+} else {
+    console.log('Você é estrangeiro.')
+}
