0a3a3c326b5fe868b2b3775fdb1a8b221fd0fd9d
brunomcardoso151986 <brunomcardoso151986@gmail.com>
2020-08-02 01:44:44 -0300

    commit exerciciosJSP2.html
    Subindo o arquivo exerciciosJSP2.html com a atualização do arquivo script.js e imagens utilizadas no exercício.

--- script.js
+++ script.js
@@ -19,3 +19,61 @@ function carregar() {
         document.body.style.background = '#291d35'
     }
 }
+
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtAno')
+    var res = document.querySelector('div#res')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 2) {
+                //Bebê
+                img.setAttribute('src', 'homem_bebe.png')
+            } else if (idade >= 2 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'homem_crianca.png')
+            } else if (idade >= 10 && idade < 18) {
+                //Adolescente
+                img.setAttribute('src', 'homem_adolescente.png')
+            } else if (idade >= 18 && idade < 65) {
+                //Adulto
+                img.setAttribute('src', 'homem_adulto.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'homem_idoso.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 2) {
+                //Bebê
+                img.setAttribute('src', 'mulher_bebe.png')
+            } else if (idade >= 2 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'mulher_crianca.png')
+            } else if (idade >= 10 && idade < 18) {
+                //Adolescente
+                img.setAttribute('src', 'mulher_adolescente.png')
+            } else if (idade >= 18 && idade < 65) {
+                //Adulta
+                img.setAttribute('src', 'mulher_adulta.png')
+            } else {
+                //Idosa
+                img.setAttribute('src', 'mulher_idosa.png')
+            }
+        }
+
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
